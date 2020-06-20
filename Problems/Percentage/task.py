b69f078b31962761c4c4085f93050f052ef32e02
codewithgsp <codewithgsp@gmail.com>
2020-06-20 17:49:37 +0200

    Percentage

--- Problems/Percentage/task.py
+++ Problems/Percentage/task.py
@@ -1,2 +1,8 @@
-def get_percentage():
-    pass
+def get_percentage(num, round_digits=None):
+    return str(round(num * 100, ndigits=round_digits)) + '%'
+
+
+print(get_percentage(0.0123))      # 1%
+print(get_percentage(0.0123, 0))   # 1.0%
+print(get_percentage(0.0123, 1))   # 1.2%
+print(get_percentage(0.0123, 10))  # 1.23%
